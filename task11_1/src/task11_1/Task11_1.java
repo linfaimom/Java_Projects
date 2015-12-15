e0857800717fcb87f7130f1f34e3c770d4cc51dd
marcus <linfaimom@gmail.com>
2015-12-15 10:04:04 +0800

    finished the task9_8 and task11_1

---
+++ task11_1/src/task11_1/Task11_1.java
@@ -0,0 +1,58 @@
+//测试GUI图形用户界面程序
+
+package task11_1;
+
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+class FirstWindow extends JFrame{
+	JMenuBar menuBar;  					//菜单条
+	JMenu menuFile;						//菜单“File”
+	JMenuItem menuItemCreate;			//菜单项“Create”
+	JMenu menuOpen;						//菜单“Open”
+	JMenuItem menuItemSave;				//菜单项“Save”
+	JMenuItem menuItemClose;			//菜单项“Close”
+	JMenuItem menuItemFile1;			//菜单项“File1”
+	JMenuItem menuItemFile2;	        //菜单项“File2”
+	JButton btnOK;      	            //按钮OK
+	
+	public FirstWindow(){
+		setLayout(new FlowLayout()); 	//设置窗口布局为FlowLayout
+		init();							//初始化各对象
+		setVisible(true); 				//设置可见
+		setSize(640,360);
+	}
+	
+	//初始化各对象
+	public void init(){
+		menuBar = new JMenuBar();  							//菜单条
+		menuFile = new JMenu("File");						//菜单“File”
+		menuItemCreate = new JMenuItem("Create");			//菜单项“Create”
+		menuOpen = new JMenu("Open");						//菜单“Open”
+		menuItemSave = new JMenuItem("Save");				//菜单项“Save”
+		menuItemClose = new JMenuItem("Close");				//菜单项“Close”
+		menuItemFile1 = new JMenuItem("File1");				//菜单项“File1”
+		menuItemFile2 = new JMenuItem("File2");	        	//菜单项“File2”
+		btnOK = new JButton("OK");      	        		//按钮OK
+		menuBar.add(menuFile);
+		menuBar.add(menuOpen);
+		menuFile.add(menuItemCreate);
+		menuFile.add(menuItemClose);
+		menuFile.add(menuItemSave);
+		menuFile.add(menuOpen);
+		menuOpen.add(menuItemFile1);
+		menuOpen.add(menuItemFile2);
+		add(menuBar);
+		add(btnOK);
+	}
+}
+
+public class Task11_1 {
+	public static void main(String[] args) {
+		FirstWindow first = new FirstWindow();
+	}
+}
---
+++ task9_8/src/task9_8/Task9_8.java
@@ -0,0 +1,57 @@
+//测试RandomAccessFile随机读写类
+
+package task9_8;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class Task9_8 {
+	public static void main(String[] args) {
+		try {
+			//创建对象，支持读和写
+			RandomAccessFile raf = new RandomAccessFile("d:\\data.dat","rw");
+			
+			//创建10个随机数并写入文件
+			for (int i = 0; i < 10; i++) {
+				int n = (int)(Math.random()*90+10);
+				raf.writeInt(n);
+			}
+			
+			//选择排序
+			for (int i = 0; i < 9; i++) {
+				//定位到第i个数
+				raf.seek(i*4);
+				//读取一个整数到iData
+				int iData = raf.readInt();
+				for (int j = i+1; j < 10; j++) {
+					raf.seek(j*4);
+					int jData = raf.readInt();
+					if(jData>iData){
+						//若jData较大，则把iData位置的数据覆盖为jData，由此可见本程序是按从大到小顺序排列
+						raf.seek(i*4);
+						raf.writeInt(jData);
+						raf.seek(j*4);
+						raf.writeInt(iData);
+						//让iData记录两者中的较大值
+						iData = jData;
+					}
+				}
+			}
+			
+			//从文件中读取排序后的数字，输出到屏幕
+			for (int i = 0; i < 10; i++) {
+				raf.seek(i*4);
+				int read = raf.readInt();
+				System.out.println(read);	
+			}
+			
+			//关闭随机读写
+			raf.close();
+		} catch (FileNotFoundException e) {
+			e.getMessage();
+		} catch (IOException e) {
+			e.getMessage();
+		}
+	}
+}
